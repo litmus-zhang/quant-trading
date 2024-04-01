bc9d0df0d538972a1b8a341bddf72ff4d02ce13e
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-04-01 20:45:34 +0000

    updated the data fetching logic

--- ch-1/SnR.py
+++ ch-1/SnR.py
@@ -1,91 +1,92 @@
-import numpy as np
-import pandas as pd
-from pandas_datareader import data as pdr
-import yfinance as yf
-import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# from pandas_datareader import data as pdr
+# import yfinance as yf
+# import matplotlib.pyplot as plt
 
-start_date = '2014-01-01'
-end_date = '2018-01-01'
-SRC_DATA = 'goog_data.pkl'
-yf.pdr_override()
-
-
-def getData():
-    try:
-        goog_data = pd.read_pickle(SRC_DATA)
-        print("File data found...reading GOOG data")
-    except FileNotFoundError:
-        print("File not found, downloading GOOG data....")
-        goog_data = pdr.get_data_yahoo("GOOG", start=start_date, end=end_date)
-        goog_data.to_pickle(SRC_DATA)
-    goog_data_signal = pd.DataFrame(index=goog_data.index)
-    goog_data_signal['price'] = goog_data['price']
-    lows = goog_data['Low']
-    highs = goog_data['High']
-    return goog_data_signal, lows, highs
+# start_date = '2014-01-01'
+# end_date = '2018-01-01'
+# SRC_DATA = 'goog_data.pkl'
+# yf.pdr_override()
 
 
-def plotGraph():
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111, ylabel="Google price in $")
-    data, highs, lows = getData()
-    highs.plot(ax=ax1, color='c', lw=2.)
-    lows.plot(ax=ax1, color='y', lw=2.)
-    plt.hlines(highs.head(200).max(), lows.index.values[0], lows.index.values[-1], linewidth=2, color='g')
-    plt.hlines(lows.head(200).min(), lows.index.values[0], lows.index.values[-1], linewidth=2, color='r')
-    plt.axvline(linewidth=2, color='b', x=lows.index.values[200], linestyle=':')
-    plt.show()
+# def getData():
+#     try:
+#         goog_data = pd.read_pickle(SRC_DATA)
+#         print("File data found...reading GOOG data")
+#     except FileNotFoundError:
+#         print("File not found, downloading GOOG data....")
+#         goog_data = pdr.get_data_yahoo("GOOG", start=start_date, end=end_date)
+#         goog_data.to_pickle(SRC_DATA)
+#     goog_data_signal = pd.DataFrame(index=goog_data.index)
+#     print(goog_data_signal.head())
+#     goog_data_signal['price'] = goog_data['price']
+#     lows = goog_data['Low']
+#     highs = goog_data['High']
+#     return goog_data_signal, lows, highs
 
 
-def main():
-    return plotGraph()
+# def plotGraph():
+#     fig = plt.figure()
+#     ax1 = fig.add_subplot(111, ylabel="Google price in $")
+#     data, highs, lows = getData()
+#     highs.plot(ax=ax1, color='c', lw=2.)
+#     lows.plot(ax=ax1, color='y', lw=2.)
+#     plt.hlines(highs.head(200).max(), lows.index.values[0], lows.index.values[-1], linewidth=2, color='g')
+#     plt.hlines(lows.head(200).min(), lows.index.values[0], lows.index.values[-1], linewidth=2, color='r')
+#     plt.axvline(linewidth=2, color='b', x=lows.index.values[200], linestyle=':')
+#     plt.show()
 
 
-def trading_support_resistence(data, bin_width=20):
-    # data,_,_ = getData()
-    data['sup_tolerance'] = pd.Series(np.zeros(len(data)))
-    data['res_tolerance'] = pd.Series(np.zeros(len(data)))
-    data['sup_count'] = pd.Series(np.zeros(len(data)))
-    data['res_count'] = pd.Series(np.zeros(len(data)))
-    data['sup'] = pd.Series(np.zeros(len(data)))
-    data['res'] = pd.Series(np.zeros(len(data)))
-    data['positions'] = pd.Series(np.zeros(len(data)))
-    data['signal'] = pd.Series(np.zeros(len(data)))
-    in_support = 0
-    in_resistance = 0
-
-    for x in range((bin_width - 1) + bin_width, len(data)):
-        data_Section = data[x - bin_width:x + 1]
-        support_level = min(data_Section['price'])
-        resistance_level = max(data_Section['price'])
-        range_level = resistance_level - support_level
-        data['res'][x] = resistance_level
-        data['sup'][x] = support_level
-        data['sup_tolerance'][x] = support_level + 0.2 * range_level
-        data['res_tolerance'][x] = resistance_level - 0.2 * range_level
-
-        if data['res_tolerance'][x] <= data['price'][x] <= data['res'][x]:
-            in_resistance += 1
-            data['res_count'][x] = in_resistance
-        elif data['sup_tolerance'][x] >= data['price'][x] >= data['sup'][x]:
-            in_support += 1
-            data['sup_count'][x] = in_support
-        else:
-            in_support = in_resistance = 0
-        if in_resistance > 2:
-            data['signal'][x] = 1
-        elif in_support > 2:
-            data['signal'][x] = 0
-
-        else:
-            data['signal'][x] = data['signal'][x - 1]
-    data['positions'] = data['signal'].diff()
+# def main():
+#     return plotGraph()
 
 
-data, _, _ = getData()
-trading_support_resistence(data=data)
+# def trading_support_resistence(data, bin_width=20):
+#     # data,_,_ = getData()
+#     data['sup_tolerance'] = pd.Series(np.zeros(len(data)))
+#     data['res_tolerance'] = pd.Series(np.zeros(len(data)))
+#     data['sup_count'] = pd.Series(np.zeros(len(data)))
+#     data['res_count'] = pd.Series(np.zeros(len(data)))
+#     data['sup'] = pd.Series(np.zeros(len(data)))
+#     data['res'] = pd.Series(np.zeros(len(data)))
+#     data['positions'] = pd.Series(np.zeros(len(data)))
+#     data['signal'] = pd.Series(np.zeros(len(data)))
+#     in_support = 0
+#     in_resistance = 0
+
+#     for x in range((bin_width - 1) + bin_width, len(data)):
+#         data_Section = data[x - bin_width:x + 1]
+#         support_level = min(data_Section['price'])
+#         resistance_level = max(data_Section['price'])
+#         range_level = resistance_level - support_level
+#         data['res'][x] = resistance_level
+#         data['sup'][x] = support_level
+#         data['sup_tolerance'][x] = support_level + 0.2 * range_level
+#         data['res_tolerance'][x] = resistance_level - 0.2 * range_level
+
+#         if data['res_tolerance'][x] <= data['price'][x] <= data['res'][x]:
+#             in_resistance += 1
+#             data['res_count'][x] = in_resistance
+#         elif data['sup_tolerance'][x] >= data['price'][x] >= data['sup'][x]:
+#             in_support += 1
+#             data['sup_count'][x] = in_support
+#         else:
+#             in_support = in_resistance = 0
+#         if in_resistance > 2:
+#             data['signal'][x] = 1
+#         elif in_support > 2:
+#             data['signal'][x] = 0
+
+#         else:
+#             data['signal'][x] = data['signal'][x - 1]
+#     data['positions'] = data['signal'].diff()
 
 
-def plotSnR():
-    fig= plt.figure()
-    ax1= fig.add_subplot(111,ylabel='Google price in $')
+# data, _, _ = getData()
+# trading_support_resistence(data=data)
+
+
+# def plotSnR():
+#     fig= plt.figure()
+#     ax1= fig.add_subplot(111,ylabel='Google price in $')
--- ch-1/buyLowSellHigh.py
+++ ch-1/buyLowSellHigh.py
@@ -1,47 +1,47 @@
 import numpy
 import matplotlib.pyplot as plt
-from pandas_datareader import data as pdr
-import yfinance as yf
+import pandas_datareader.data as pdr
 import pandas as pd
+import datetime as dt
 
-from SnR import getData
+# from SnR import getData
 
-start_date = '2014-01-01'
-end_date = '2018-01-01'
+start_date = dt.datetime(2020, 1, 29)
+end_date = dt.datetime.today()
 
-yf.pdr_override()
+goog_data = pdr.DataReader('GOOG', 'yahoo-actions', start_date, end_date)
+goog_data.head()
+# print(goog_data.head(), goog_data)
+# def main():
+#     goog_data = getData()
+#     goog_data_signal = pd.DataFrame(index=goog_data.index)
+#     goog_data_signal['price'] = goog_data['Adj Close']
+#     goog_data_signal['daily_difference'] = goog_data_signal['price'].diff()
+#     goog_data_signal['signal'] = 0.0
+#     goog_data_signal['signal'] = numpy.where(goog_data_signal['daily_difference'] > 0, 1.0, 0.0)
+#     goog_data_signal['positions'] = goog_data_signal['signal'].diff()
+
+#     # visualizing the signal
+#     fig = plt.figure()
+#     ax1 = fig.add_subplot(111, ylabel="Google price in $")
+#     goog_data_signal['price'].plot(ax=ax1, color='r', lw=2.)
+#     ax1.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index,
+#              goog_data_signal.price[goog_data_signal.positions == 1.0], "^", markersize=5, color="m")
+#     ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
+#              goog_data_signal.price[goog_data_signal.positions == -1.0], "v", markersize=5, color="m")
+#     # plt.show()
+
+#     # Bactkesting
+#     initial_capital = float(1000.0)
+#     positions = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
+#     portfolio = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
+
+#     positions['GOOG'] = goog_data_signal['signal']
+#     portfolio['positions'] = (positions.multiply(goog_data_signal['price'], axis=0))
+#     portfolio['cash'] =  initial_capital - (positions.diff().multiply(goog_data_signal['price'], axis=0)).cumsum()
+#     portfolio['total'] = portfolio['positions'] + portfolio['cash']
+
+#     print(portfolio)
 
 
-def main():
-    goog_data = getData()
-    goog_data_signal = pd.DataFrame(index=goog_data.index)
-    goog_data_signal['price'] = goog_data['Adj Close']
-    goog_data_signal['daily_difference'] = goog_data_signal['price'].diff()
-    goog_data_signal['signal'] = 0.0
-    goog_data_signal['signal'] = numpy.where(goog_data_signal['daily_difference'] > 0, 1.0, 0.0)
-    goog_data_signal['positions'] = goog_data_signal['signal'].diff()
-
-    # visualizing the signal
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111, ylabel="Google price in $")
-    goog_data_signal['price'].plot(ax=ax1, color='r', lw=2.)
-    ax1.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index,
-             goog_data_signal.price[goog_data_signal.positions == 1.0], "^", markersize=5, color="m")
-    ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
-             goog_data_signal.price[goog_data_signal.positions == -1.0], "v", markersize=5, color="m")
-    # plt.show()
-
-    # Bactkesting
-    initial_capital = float(1000.0)
-    positions = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
-    portfolio = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
-
-    positions['GOOG'] = goog_data_signal['signal']
-    portfolio['positions'] = (positions.multiply(goog_data_signal['price'], axis=0))
-    portfolio['cash'] =  initial_capital - (positions.diff().multiply(goog_data_signal['price'], axis=0)).cumsum()
-    portfolio['total'] = portfolio['positions'] + portfolio['cash']
-
-    print(portfolio)
-
-
-main()
+# main()
